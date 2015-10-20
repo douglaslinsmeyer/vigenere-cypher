c4815826cf6a96e7943598ce383508138ca5ee20
Douglas Linsmeyer <dlinsmey@nerdery.com>
2015-10-20 01:26:46 -0700

    Removes work from constructor.

--- src/Encrypter.php
+++ src/Encrypter.php
@@ -17,12 +17,66 @@ class Encrypter
     /**
      * @var array
      */
-    private $characterToValueMap;
+    private $characterToValueMap = [
+        'A' => 0,
+        'B' => 1,
+        'C' => 2,
+        'D' => 3,
+        'E' => 4,
+        'F' => 5,
+        'G' => 6,
+        'H' => 7,
+        'I' => 8,
+        'J' => 9,
+        'K' => 10,
+        'L' => 11,
+        'M' => 12,
+        'N' => 13,
+        'O' => 14,
+        'P' => 15,
+        'Q' => 16,
+        'R' => 17,
+        'S' => 18,
+        'T' => 19,
+        'U' => 20,
+        'V' => 21,
+        'W' => 22,
+        'X' => 23,
+        'Y' => 24,
+        'Z' => 25,
+    ];
 
     /**
      * @var array
      */
-    private $valueToCharacterMap;
+    private $valueToCharacterMap = [
+        0 => 'A',
+        1 => 'B',
+        2 => 'C',
+        3 => 'D',
+        4 => 'E',
+        5 => 'F',
+        6 => 'G',
+        7 => 'H',
+        8 => 'I',
+        9 => 'J',
+        10 => 'K',
+        11 => 'L',
+        12 => 'M',
+        13 => 'N',
+        14 => 'O',
+        15 => 'P',
+        16 => 'Q',
+        17 => 'R',
+        18 => 'S',
+        19 => 'T',
+        20 => 'U',
+        21 => 'V',
+        22 => 'W',
+        23 => 'X',
+        24 => 'Y',
+        25 => 'Z',
+    ];
 
     /**
      * @var string
@@ -43,8 +97,6 @@ class Encrypter
     {
         $this->key = strtoupper($key);
         $this->keyLength = strlen($key);
-        $this->valueToCharacterMap = range('A', 'Z');
-        $this->characterToValueMap = array_flip($this->valueToCharacterMap);
     }
 
     /**
